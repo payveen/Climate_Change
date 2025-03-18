3d25a2ad298044836184869c8a4618f9a2531599
payvven <paiveenessa04@gmail.com>
2025-03-18 06:38:20 +0300

    payveen

---
+++ app/src/main/java/com/example/climatechange/selectionActivity.java
@@ -0,0 +1,38 @@
+package com.example.climatechange;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class selectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_selection);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void selectionTOtrash(View view) {
+        Intent in=new Intent(this,trashActivity.class);
+        startActivity(in);
+        finish();
+    }
+
+    public void selectionTOtree(View view) {
+        Intent in=new Intent(this,treeActivity.class);
+        startActivity(in);
+        finish();
+    }
+}
